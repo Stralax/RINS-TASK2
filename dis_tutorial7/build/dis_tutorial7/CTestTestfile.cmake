6df29a5da7cada7b2b093fa48518f6e6c06d408e
ddjorlev <ddzorlev@yahoo.com>
2025-06-01 13:14:09 +0200

    depth normala first try

--- dis_tutorial7/build/dis_tutorial7/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/beta/RINS-TASK2/dis_tutorial7
-# Build directory: /home/beta/RINS-TASK2/dis_tutorial7/build/dis_tutorial7
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
